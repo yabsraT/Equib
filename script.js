613851bfcef2dfe7496d59299f3652dd483b983f
yabsraT <101882130+yabsraT@users.noreply.github.com>
2022-12-10 08:38:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const name =document.getElementById("name").value;
+const amount =document.getElementById("amount").value;
+
+
+int list= []
+list.append(name);
+const 
+// insert gender 
+// add chart 
+//
